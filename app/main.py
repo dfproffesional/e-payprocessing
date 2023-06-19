9b52206db0d808f2eb6e17a442f42d7cbf8635f3
Daniel F <dfproffesional@gmail.com>
2023-06-19 04:23:49 +0000

    Update: Separate Services

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from middleware import ErrorHandler
+
+from router import icl_router
 
 app = FastAPI()
+app.include_router(icl_router)
+app.add_middleware(ErrorHandler)
 
 @app.get('/')
 def message():
-    return "Hello World"
+    return "Hello World, default message"
