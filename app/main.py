d582704a37e825cdc1656e7f911101161dd08f72
Daniel F <dfproffesional@gmail.com>
2023-06-21 03:39:39 +0000

    Add: First Model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from router import routes
 from fastapi import FastAPI
 from middleware import ErrorHandler
+from config.database import Database
+from model.file_header import FileHeader
 
 app = FastAPI()
 app.include_router(routes)
 app.add_middleware(ErrorHandler)
 
+Database().metadata.create_all(bind=Database().engine())
+
 @app.get('/')
 def message():
-    return "Hello World, default message"
+    return FileHeader().test()
